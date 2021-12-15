40c5f1a8f43bedcc438b9288493d1d5d840d24cc
Pebetha <95303101+Pebetha@users.noreply.github.com>
2021-12-15 11:54:18 +0700

    Add files via upload

---
+++ 12-15-2021/AppV1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//import TextInputReview from './components/TextInputReview';
+//import ImageWithTextInput from './components/ImageWithTextInput';
+//import ButtonExample from './components/ButtonExample';
+//import TextInputPractice1 from './components/TextInputPractice1';
+//import TouchableExample from './components/TouchableExample';
+//import TextInputPractice1 from './components/TextInputPractice1';
+//import TouchablePractice from './components/TouchablePractice';
+import Logo from './components/Logo';
+const App = () => {
+  return (  
+    //<TextInputReview/>
+    //<ImageWithTextInput/>
+    //<ButtonExample/>
+    //<TextInputPractice1/>
+    //<TouchableExample/>
+    //<TouchablePractice/>
+    <Logo/>
+  );
+};
+
+export default App;
