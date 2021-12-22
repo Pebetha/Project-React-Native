0376073107183febfb04643bd2e515eac021c029
Pebetha <95303101+Pebetha@users.noreply.github.com>
2021-12-22 12:08:57 +0700

    WebView and Stack Navigation

---
+++ 22-12-2021/AppV3.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import ModelExample from './components/ModelExample'
+import WebViewExample from './components/WebViewExample'
+import WebViewLoading from './components/WebViewLoading'
+//import AlertExample from './components/AlertExample'
+
+const App = () => {
+  return (
+
+      //<AlertExample/>
+      //<ModelExample/>
+      //<WebViewExample/>
+      <WebViewLoading/>
+
+  )
+}
+
+export default App
